fdafdec89db0717e287945d95805c462a731af5a
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2022-03-20 01:26:05 -0300

    feat: reaction models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 from accounts.models import User
@@ -12,3 +13,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class Reaction(models.Model):
+    like = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
