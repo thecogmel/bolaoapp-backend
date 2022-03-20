4fb89823e681ba7779b9975267b2001f61a93739
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2022-03-20 01:26:32 -0300

    refactor: extend token time

--- bolao/settings.py
+++ bolao/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
